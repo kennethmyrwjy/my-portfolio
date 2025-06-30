8aac2249ab2bad8fc67661469ee6b5c6a7c1ed73
kennethmyrwjy <zenomender@gmail.com>
2025-06-30 14:03:21 +0700

    wip3

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.tab-button').forEach(button => {
+    button.addEventListener('click', function() {
+        document.querySelectorAll('.tab-button').forEach(btn => btn.classList.remove('active'));
+        this.classList.add('active');
+    });
+});
